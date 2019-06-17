8b8fda38c58e0b944ae936651b2d4aa4461aea5f
mrbird <852252810@qq.com>
2019-06-17 19:08:59 +0800

    update

--- src/main/java/cc/mrbird/febs/common/handler/GlobalExceptionHandler.java
+++ src/main/java/cc/mrbird/febs/common/handler/GlobalExceptionHandler.java
@@ -39,7 +39,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = FebsException.class)
     public FebsResponse handleParamsInvalidException(FebsException e) {
-        log.error("系统错误", e);
+        log.error("系统错误{}", e.getMessage());
         return new FebsResponse().code(HttpStatus.INTERNAL_SERVER_ERROR).message(e.getMessage());
     }
 
